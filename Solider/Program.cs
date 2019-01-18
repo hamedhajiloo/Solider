d8851c14273b048209347e3d0930aa4b3358abe1
hamed <hamedhajiloo96@gmail.com>
2019-01-18 22:45:38 +0330

    Add project files.

---
+++ Solider/Program.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solider
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            int n =int.Parse( Console.ReadLine());
+           // var g = 2 Math (n + 1);
+            var len = (2 ^ (n + 1)) - 1;
+            char[,] res = new char[len,len];
+
+
+            for (int i = 0; i < len; i++)
+            {
+                for (int j = 0; j < len; j++)
+                {
+                    res[0,j] = '#';
+                    res[i,0] = '#';
+                    res[i,len - 1] = '#';
+                    res[len - 1,j] = '#';
+                }
+            }
+
+            for (int i = 0; i < len; i++)
+            {
+                for (int j = 0; j < len; j++)
+                {
+                    Console.Write(res[i,j]);
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
